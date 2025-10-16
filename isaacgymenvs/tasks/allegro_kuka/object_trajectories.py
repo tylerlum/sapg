3d59d78ab80478794987f1620ff21e5a76adf1d6
Kushal Kedia <kedia@bohg-ws-16.stanford.edu>
2025-10-15 19:08:16 -0700

    Add new hammering motion

--- isaacgymenvs/tasks/allegro_kuka/object_trajectories.py
+++ isaacgymenvs/tasks/allegro_kuka/object_trajectories.py
@@ -10,27 +10,34 @@ def get_hammer_trajectory(object_init_state, device="cuda"):
     pick_up_state[3:7] = R.from_euler('y', 0, degrees=True).as_quat()
     pick_up_state[2] += 0.2
 
-    # next state rotates 90 degrees around y axis
+    # next state rotates -90 degrees around x axis wrt last state
     rotate_90_state = pick_up_state.copy()
-    rotate_90_state[3:7] = R.from_euler('y', 90, degrees=True).as_quat()
+    rotate_90_state[3:7] = (R.from_quat(pick_up_state[3:7]) * R.from_euler('x', -90, degrees=True)).as_quat()
 
-    # next state rotates 20 degrees around x axis
+    trajectory_states = [pick_up_state, rotate_90_state]
+    # next state rotates 20 degrees around z axis wrt last state
     swing_up_state = rotate_90_state.copy()
-    swing_up_state[3:7] = R.from_euler('x', 20, degrees=True).as_quat()
+    swing_up_state[3:7] = (R.from_quat(rotate_90_state[3:7]) * R.from_euler('z', -20, degrees=True)).as_quat()
 
-    # next state rotates -40 degrees around x axis and swings hammer down
+    # next state rotates -40 degrees around x axis and swings hammer down wrt last state
     swing_down_state = swing_up_state.copy()
-    swing_down_state[3:7] = R.from_euler('x', -40, degrees=True).as_quat()
-    swing_down_state[2] -= 0.1
+    swing_down_state[3:7] = (R.from_quat(swing_up_state[3:7]) * R.from_euler('z', 60, degrees=True)).as_quat()
+    swing_down_state[2] -= 0.15
 
-    trajectory_states = [pick_up_state, rotate_90_state, swing_up_state, 
-            swing_down_state, swing_up_state, swing_down_state]
+    num_swings = 4
+    for _ in range(num_swings):
+        trajectory_states.append(swing_up_state)
+        trajectory_states.append(swing_down_state)
+
+    # trajectory_states = [pick_up_state, rotate_90_state, swing_up_state, 
+    #         swing_down_state, swing_up_state, swing_down_state]
     return torch.tensor(trajectory_states, dtype=torch.float32, device=device)
 
 def get_screwdriver_trajectory(object_init_state, device="cuda"):
     trajectory_states = []
     # first state is pick up state
     pick_up_state = object_init_state.copy()
+    pick_up_state[3:7] = R.from_euler('y', 0, degrees=True).as_quat()
     pick_up_state[2] += 0.2
 
     # next state rotates 90 degrees around y axis
