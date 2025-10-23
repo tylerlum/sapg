974a2f5f9ed84c44352d2bbf6da8cd6669497ae8
Tyler Lum <tylergwlum@gmail.com>
2025-10-23 16:15:03 -0700

    Fix pu

--- mujoco/viser_xml_v2.py
+++ mujoco/viser_xml_v2.py
@@ -22,7 +22,10 @@ def create_mesh_handles(
         is_collision = mj_model.geom_contype[i] != 0 or mj_model.geom_conaffinity[i] != 0
 
         # Add geom to body's list if it matches the type we're looking for
-        if (mesh_type == "collision" and is_collision) or (mesh_type == "visual" and not is_collision):
+        if not any([
+            (mesh_type == "collision" and is_collision),
+            (mesh_type == "visual" and not is_collision),
+        ]):
             continue
         if body_id not in body_geoms:
             body_geoms[body_id] = []
@@ -83,6 +86,28 @@ def update_mujoco(
 
     server.flush()
 
+IIWA_INIT_JOINT_POS = np.array([-1.571, 1.571, -0.000, 1.376, -0.000, 1.485, 2.358])
+IIWA_JOINT_NAMES = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6', 'joint7',]
+IIWA_ACTUATOR_NAMES = ['actuator1', 'actuator2', 'actuator3', 'actuator4', 'actuator5', 'actuator6', 'actuator7',]
+assert len(IIWA_INIT_JOINT_POS) == len(IIWA_JOINT_NAMES) == len(IIWA_ACTUATOR_NAMES) == 7, f"len(IIWA_INIT_JOINT_POS): {len(IIWA_INIT_JOINT_POS)}, len(IIWA_JOINT_NAMES): {len(IIWA_JOINT_NAMES)}, len(IIWA_ACTUATOR_NAMES): {len(IIWA_ACTUATOR_NAMES)}, expected: 7"
+
+ALLEGRO_INIT_JOINT_POS = np.zeros(16)
+ALLEGRO_INIT_JOINT_POS[12] = 0.3
+ALLEGRO_JOINT_NAMES = ['palmffj0', 'palmffj1', 'palmffj2', 'palmffj3', 'palmmfj0', 'palmmfj1', 'palmmfj2', 'palmmfj3', 'palmrfj0', 'palmrfj1', 'palmrfj2', 'palmrfj3', 'palmthj0', 'palmthj1', 'palmthj2', 'palmthj3']
+ALLEGRO_ACTUATOR_NAMES = ['palmffa0', 'palmffa1', 'palmffa2', 'palmffa3', 'palmmfa0', 'palmmfa1', 'palmmfa2', 'palmmfa3', 'palmrfa0', 'palmrfa1', 'palmrfa2', 'palmrfa3', 'palmtha0', 'palmtha1', 'palmtha2', 'palmtha3']
+assert len(ALLEGRO_INIT_JOINT_POS) == len(ALLEGRO_JOINT_NAMES) == len(ALLEGRO_ACTUATOR_NAMES) == 16, f"len(ALLEGRO_INIT_JOINT_POS): {len(ALLEGRO_INIT_JOINT_POS)}, len(ALLEGRO_JOINT_NAMES): {len(ALLEGRO_JOINT_NAMES)}, len(ALLEGRO_ACTUATOR_NAMES): {len(ALLEGRO_ACTUATOR_NAMES)}, expected: 16"
+
+INIT_JOINT_POS = np.concatenate([IIWA_INIT_JOINT_POS, ALLEGRO_INIT_JOINT_POS])
+JOINT_NAMES = IIWA_JOINT_NAMES + ALLEGRO_JOINT_NAMES
+ACTUATOR_NAMES = IIWA_ACTUATOR_NAMES + ALLEGRO_ACTUATOR_NAMES
+assert len(INIT_JOINT_POS) == len(JOINT_NAMES) == len(ACTUATOR_NAMES) == 23, f"len(INIT_JOINT_POS): {len(INIT_JOINT_POS)}, len(JOINT_NAMES): {len(JOINT_NAMES)}, len(ACTUATOR_NAMES): {len(ACTUATOR_NAMES)}, expected: 23"
+
+
+def sim_step(mj_model: mujoco.MjModel, mj_data: mujoco.MjData):
+    actuator_ids = [mj_model.actuator(name=name).id for name in ACTUATOR_NAMES]
+    for i, actuator_id in enumerate(actuator_ids):
+        mj_data.ctrl[actuator_id] = INIT_JOINT_POS[i]
+    mujoco.mj_step(mj_model, mj_data)
 
 
 server = viser.ViserServer()
@@ -102,14 +127,12 @@ collision_handles = create_mesh_handles(
 
 mujoco.mj_step(mj_model, mj_data)
 
-body_names = [mj_model.body(i).name for i in range(mj_model.nbody)]
-body_ids = [mj_model.body(name=name).id for name in body_names]
-body_xpositions = [mj_data.xpos[body_id] for body_id in body_ids]
-body_xquats = [mj_data.xquat[body_id] for body_id in body_ids]
-body_xmats = [mj_data.xmat[body_id].reshape(3, 3) for body_id in body_ids]
-breakpoint()
-
 while True:
+    body_names = [mj_model.body(i).name for i in range(mj_model.nbody)]
+    body_ids = [mj_model.body(name=name).id for name in body_names]
+    body_xpositions = [mj_data.xpos[body_id] for body_id in body_ids]
+    body_xquats = [mj_data.xquat[body_id] for body_id in body_ids]
+    body_xmats = [mj_data.xmat[body_id].reshape(3, 3) for body_id in body_ids]
     update_mujoco(
         server=server,
         visual_handles=visual_handles,
@@ -117,5 +140,7 @@ while True:
         body_xpos=np.array(body_xpositions)[None],
         body_xmat=np.array(body_xmats)[None],
     )
-    print("Sleeping for 1.0 seconds")
-    time.sleep(1.0)
+    sim_step(mj_model, mj_data)
+    dt = 1/60
+    print(f"Sleeping for {dt} seconds")
+    time.sleep(dt)
