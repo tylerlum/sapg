b79ab593e5b60792040b63cd5ed0f7f225d1bd99
Tyler Lum <tylergwlum@gmail.com>
2025-10-23 16:40:05 -0700

    Fix up sim.py

--- mujoco/sim.py
+++ mujoco/sim.py
@@ -1,35 +1,137 @@
 from __future__ import annotations
-import mujoco
+
+import time
+from dataclasses import dataclass, field
+from pathlib import Path
+
 import mujoco.viewer
 import numpy as np
-import time
-from pathlib import Path
-from dataclasses import dataclass, field
 
+import mujoco
 
 # Robot constants
 N_IIWA_JOINTS = 7
 IIWA_INIT_JOINT_POS = np.array([-1.571, 1.571, -0.000, 1.376, -0.000, 1.485, 2.358])
-IIWA_JOINT_NAMES = ['joint1', 'joint2', 'joint3', 'joint4', 'joint5', 'joint6', 'joint7',]
-IIWA_ACTUATOR_NAMES = ['actuator1', 'actuator2', 'actuator3', 'actuator4', 'actuator5', 'actuator6', 'actuator7',]
-assert len(IIWA_INIT_JOINT_POS) == len(IIWA_JOINT_NAMES) == len(IIWA_ACTUATOR_NAMES) == 7, f"len(IIWA_INIT_JOINT_POS): {len(IIWA_INIT_JOINT_POS)}, len(IIWA_JOINT_NAMES): {len(IIWA_JOINT_NAMES)}, len(IIWA_ACTUATOR_NAMES): {len(IIWA_ACTUATOR_NAMES)}, expected: 7"
+IIWA_JOINT_NAMES = [
+    "joint1",
+    "joint2",
+    "joint3",
+    "joint4",
+    "joint5",
+    "joint6",
+    "joint7",
+]
+IIWA_ACTUATOR_NAMES = [
+    "actuator1",
+    "actuator2",
+    "actuator3",
+    "actuator4",
+    "actuator5",
+    "actuator6",
+    "actuator7",
+]
+assert (
+    len(IIWA_INIT_JOINT_POS) == len(IIWA_JOINT_NAMES) == len(IIWA_ACTUATOR_NAMES) == 7
+), (
+    f"len(IIWA_INIT_JOINT_POS): {len(IIWA_INIT_JOINT_POS)}, len(IIWA_JOINT_NAMES): {len(IIWA_JOINT_NAMES)}, len(IIWA_ACTUATOR_NAMES): {len(IIWA_ACTUATOR_NAMES)}, expected: 7"
+)
 
 N_ALLEGRO_JOINTS = 16
 ALLEGRO_INIT_JOINT_POS = np.zeros(16)
 ALLEGRO_INIT_JOINT_POS[12] = 0.3
-ALLEGRO_JOINT_NAMES = ['palmffj0', 'palmffj1', 'palmffj2', 'palmffj3', 'palmmfj0', 'palmmfj1', 'palmmfj2', 'palmmfj3', 'palmrfj0', 'palmrfj1', 'palmrfj2', 'palmrfj3', 'palmthj0', 'palmthj1', 'palmthj2', 'palmthj3']
-ALLEGRO_ACTUATOR_NAMES = ['palmffa0', 'palmffa1', 'palmffa2', 'palmffa3', 'palmmfa0', 'palmmfa1', 'palmmfa2', 'palmmfa3', 'palmrfa0', 'palmrfa1', 'palmrfa2', 'palmrfa3', 'palmtha0', 'palmtha1', 'palmtha2', 'palmtha3']
-assert len(ALLEGRO_INIT_JOINT_POS) == len(ALLEGRO_JOINT_NAMES) == len(ALLEGRO_ACTUATOR_NAMES) == 16, f"len(ALLEGRO_INIT_JOINT_POS): {len(ALLEGRO_INIT_JOINT_POS)}, len(ALLEGRO_JOINT_NAMES): {len(ALLEGRO_JOINT_NAMES)}, len(ALLEGRO_ACTUATOR_NAMES): {len(ALLEGRO_ACTUATOR_NAMES)}, expected: 16"
+ALLEGRO_JOINT_NAMES = [
+    "palmffj0",
+    "palmffj1",
+    "palmffj2",
+    "palmffj3",
+    "palmmfj0",
+    "palmmfj1",
+    "palmmfj2",
+    "palmmfj3",
+    "palmrfj0",
+    "palmrfj1",
+    "palmrfj2",
+    "palmrfj3",
+    "palmthj0",
+    "palmthj1",
+    "palmthj2",
+    "palmthj3",
+]
+ALLEGRO_ACTUATOR_NAMES = [
+    "palmffa0",
+    "palmffa1",
+    "palmffa2",
+    "palmffa3",
+    "palmmfa0",
+    "palmmfa1",
+    "palmmfa2",
+    "palmmfa3",
+    "palmrfa0",
+    "palmrfa1",
+    "palmrfa2",
+    "palmrfa3",
+    "palmtha0",
+    "palmtha1",
+    "palmtha2",
+    "palmtha3",
+]
+assert (
+    len(ALLEGRO_INIT_JOINT_POS)
+    == len(ALLEGRO_JOINT_NAMES)
+    == len(ALLEGRO_ACTUATOR_NAMES)
+    == 16
+), (
+    f"len(ALLEGRO_INIT_JOINT_POS): {len(ALLEGRO_INIT_JOINT_POS)}, len(ALLEGRO_JOINT_NAMES): {len(ALLEGRO_JOINT_NAMES)}, len(ALLEGRO_ACTUATOR_NAMES): {len(ALLEGRO_ACTUATOR_NAMES)}, expected: 16"
+)
 
 N_JOINTS = N_IIWA_JOINTS + N_ALLEGRO_JOINTS
 INIT_JOINT_POS = np.concatenate([IIWA_INIT_JOINT_POS, ALLEGRO_INIT_JOINT_POS])
 JOINT_NAMES = IIWA_JOINT_NAMES + ALLEGRO_JOINT_NAMES
 ACTUATOR_NAMES = IIWA_ACTUATOR_NAMES + ALLEGRO_ACTUATOR_NAMES
-assert len(INIT_JOINT_POS) == len(JOINT_NAMES) == len(ACTUATOR_NAMES) == N_JOINTS, f"len(INIT_JOINT_POS): {len(INIT_JOINT_POS)}, len(JOINT_NAMES): {len(JOINT_NAMES)}, len(ACTUATOR_NAMES): {len(ACTUATOR_NAMES)}, expected: {N_JOINTS}"
+assert len(INIT_JOINT_POS) == len(JOINT_NAMES) == len(ACTUATOR_NAMES) == N_JOINTS, (
+    f"len(INIT_JOINT_POS): {len(INIT_JOINT_POS)}, len(JOINT_NAMES): {len(JOINT_NAMES)}, len(ACTUATOR_NAMES): {len(ACTUATOR_NAMES)}, expected: {N_JOINTS}"
+)
 
 N_BODY_NAMES = 33
-BODY_NAMES = ['world', 'base', 'link1', 'link2', 'link3', 'link4', 'link5', 'link6', 'link7', 'palmworld', 'palmpalm', 'palmff_base', 'palmff_proximal', 'palmff_medial', 'palmff_distal', 'palmff_tip', 'palmmf_base', 'palmmf_proximal', 'palmmf_medial', 'palmmf_distal', 'palmmf_tip', 'palmrf_base', 'palmrf_proximal', 'palmrf_medial', 'palmrf_distal', 'palmrf_tip', 'palmth_base', 'palmth_proximal', 'palmth_medial', 'palmth_distal', 'palmth_tip', 'table', 'object']
-assert len(BODY_NAMES) == N_BODY_NAMES, f"len(BODY_NAMES): {len(BODY_NAMES)}, expected: {N_BODY_NAMES}"
+BODY_NAMES = [
+    "world",
+    "base",
+    "link1",
+    "link2",
+    "link3",
+    "link4",
+    "link5",
+    "link6",
+    "link7",
+    "palmworld",
+    "palmpalm",
+    "palmff_base",
+    "palmff_proximal",
+    "palmff_medial",
+    "palmff_distal",
+    "palmff_tip",
+    "palmmf_base",
+    "palmmf_proximal",
+    "palmmf_medial",
+    "palmmf_distal",
+    "palmmf_tip",
+    "palmrf_base",
+    "palmrf_proximal",
+    "palmrf_medial",
+    "palmrf_distal",
+    "palmrf_tip",
+    "palmth_base",
+    "palmth_proximal",
+    "palmth_medial",
+    "palmth_distal",
+    "palmth_tip",
+    "table",
+    "object",
+]
+assert len(BODY_NAMES) == N_BODY_NAMES, (
+    f"len(BODY_NAMES): {len(BODY_NAMES)}, expected: {N_BODY_NAMES}"
+)
+
 
 @dataclass
 class FrictionConfig:
@@ -50,7 +152,13 @@ class SimulatorConfig:
 
     @property
     def friction_array(self) -> np.ndarray:
-        return np.array([self.friction.sliding_friction, self.friction.torsional_friction, self.friction.rolling_friction])
+        return np.array(
+            [
+                self.friction.sliding_friction,
+                self.friction.torsional_friction,
+                self.friction.rolling_friction,
+            ]
+        )
 
 
 class Simulator:
@@ -64,15 +172,21 @@ class Simulator:
         self.set_robot_joint_positions(INIT_JOINT_POS)
 
     def _init_scene(self) -> None:
-        iiwa_xml_path = Path("/home/tylerlum/github_repos/mujoco_menagerie/kuka_iiwa_14/scene.xml")
+        iiwa_xml_path = Path(
+            "/home/tylerlum/github_repos/mujoco_menagerie/kuka_iiwa_14/scene.xml"
+        )
         assert iiwa_xml_path.exists(), f"Robot path does not exist: {iiwa_xml_path}"
 
         # Load mjspec from robot path
         spec = mujoco.MjSpec.from_file(str(iiwa_xml_path))
         spec.option.timestep = self.config.sim_dt
 
-        allegro_xml_path = Path("/home/tylerlum/github_repos/mujoco_menagerie/wonik_allegro/right_hand_offset.xml")
-        assert allegro_xml_path.exists(), f"Allegro XML path does not exist: {allegro_xml_path}"
+        allegro_xml_path = Path(
+            "/home/tylerlum/github_repos/mujoco_menagerie/wonik_allegro/right_hand_offset.xml"
+        )
+        assert allegro_xml_path.exists(), (
+            f"Allegro XML path does not exist: {allegro_xml_path}"
+        )
         allegro_spec = mujoco.MjSpec.from_file(str(allegro_xml_path))
         attachment_site = next(s for s in spec.sites if s.name == "attachment_site")
         attachment_site.attach_body(allegro_spec.worldbody, "palm", "")
@@ -87,7 +201,9 @@ class Simulator:
         table_geom = table_body.add_geom()
         table_geom.name = "table_geom"
         table_geom.type = mujoco.mjtGeom.mjGEOM_BOX
-        table_geom.size = np.array([TABLE_LEN_X / 2, TABLE_LEN_Y / 2, TABLE_LEN_Z / 2])  # Half extents
+        table_geom.size = np.array(
+            [TABLE_LEN_X / 2, TABLE_LEN_Y / 2, TABLE_LEN_Z / 2]
+        )  # Half extents
         table_geom.pos = np.array([0.0, 0.0, 0.0])
         table_geom.rgba = WHITE_RGBA
         table_geom.friction = self.config.friction_array.copy()
@@ -138,9 +254,15 @@ class Simulator:
         print()
 
     def _validate(self) -> None:
-        assert JOINT_NAMES == self.joint_names[:N_JOINTS], f"JOINT_NAMES: {JOINT_NAMES}, self.joint_names: {self.joint_names[:N_JOINTS]}"
-        assert ACTUATOR_NAMES == self.actuator_names[:N_JOINTS], f"ACTUATOR_NAMES: {ACTUATOR_NAMES}, self.actuator_names: {self.actuator_names[:N_JOINTS]}"
-        assert BODY_NAMES == self.body_names, f"BODY_NAMES: {BODY_NAMES}, self.body_names: {self.body_names}"
+        assert JOINT_NAMES == self.joint_names[:N_JOINTS], (
+            f"JOINT_NAMES: {JOINT_NAMES}, self.joint_names: {self.joint_names[:N_JOINTS]}"
+        )
+        assert ACTUATOR_NAMES == self.actuator_names[:N_JOINTS], (
+            f"ACTUATOR_NAMES: {ACTUATOR_NAMES}, self.actuator_names: {self.actuator_names[:N_JOINTS]}"
+        )
+        assert BODY_NAMES == self.body_names, (
+            f"BODY_NAMES: {BODY_NAMES}, self.body_names: {self.body_names}"
+        )
 
     # ############################################################
     # Setting robot joint positions and targets
@@ -152,7 +274,9 @@ class Simulator:
             self.mj_data.qpos[joint_id] = q[i]
 
     def set_robot_joint_pos_targets(self, q_targets: np.ndarray) -> None:
-        assert q_targets.shape == (N_JOINTS,), f"q_targets.shape: {q_targets.shape}, expected: ({N_JOINTS},)"
+        assert q_targets.shape == (N_JOINTS,), (
+            f"q_targets.shape: {q_targets.shape}, expected: ({N_JOINTS},)"
+        )
         self.robot_joint_pos_targets = q_targets.copy()
 
     # ############################################################
@@ -160,45 +284,51 @@ class Simulator:
     # ############################################################
     def get_body_pose(self, body_name: str) -> tuple[np.ndarray, np.ndarray]:
         body_id = self.mj_model.body(name=body_name).id
-        pos = self.mj_data.xpos[body_id]   # (3,) world position of body frame
-        quat = self.mj_data.xquat[body_id] # (4,) world orientation quaternion (w, x, y, z)
-        return pos, quat
+        pos = self.mj_data.xpos[body_id]  # (3,) world position of body frame
+        quat_wxyz = self.mj_data.xquat[
+            body_id
+        ]  # (4,) world orientation quaternion (w, x, y, z)
+        return pos, quat_wxyz
 
-    def get_sim_state(self):
+    def get_sim_state(self) -> dict[str, np.ndarray]:
         # Usage:
-        table_pos, table_quat = self.get_body_pose("table")
-        object_pos, object_quat = self.get_body_pose("object")
-        robot_base_pos, robot_base_quat = self.get_body_pose("base")  # replace with actual base body name
-        joint_names = [self.mj_model.joint(i).name for i in range(self.mj_model.njnt)]
-        joint_ids = [self.mj_model.joint(name=name).id for name in joint_names]
+        table_pos, table_quat_wxyz = self.get_body_pose("table")
+        object_pos, object_quat_wxyz = self.get_body_pose("object")
+        robot_base_pos, robot_base_quat_wxyz = self.get_body_pose(
+            "base"
+        )  # replace with actual base body name
+        joint_ids = [self.mj_model.joint(name=name).id for name in JOINT_NAMES]
         joint_positions = self.mj_data.qpos[joint_ids]
         joint_velocities = self.mj_data.qvel[joint_ids]
-        actuator_names = [self.mj_model.actuator(i).name for i in range(self.mj_model.nu)]
-        body_names = [self.mj_model.body(i).name for i in range(self.mj_model.nbody)]
+        return {
+            "table_pos": table_pos,
+            "table_quat_wxyz": table_quat_wxyz,
+            "object_pos": object_pos,
+            "object_quat_wxyz": object_quat_wxyz,
+            "robot_base_pos": robot_base_pos,
+            "robot_base_quat_wxyz": robot_base_quat_wxyz,
+            "joint_positions": joint_positions,
+            "joint_velocities": joint_velocities,
+        }
 
-        PRINT = True
-        if PRINT:
-            print(f"Table pos: {table_pos}, Table quat: {table_quat}")
-            print(f"Object pos: {object_pos}, Object quat: {object_quat}")
-            print(f"Robot base pos: {robot_base_pos}, Robot base quat: {robot_base_quat}")
-            print(f"Joint names: {joint_names}")
-            print(f"Joint positions: {joint_positions}")
-            print(f"Joint velocities: {joint_velocities}")
-            print("Actuator names:", actuator_names)
-            print("Body names:", body_names)
-
+    # ############################################################
+    # Stepping simulation
+    # ############################################################
     def sim_step(self):
         actuator_ids = [self.mj_model.actuator(name=name).id for name in ACTUATOR_NAMES]
         for i, actuator_id in enumerate(actuator_ids):
             self.mj_data.ctrl[actuator_id] = self.robot_joint_pos_targets[i]
         mujoco.mj_step(self.mj_model, self.mj_data)
 
+    # ############################################################
+    # Getting body poses and simulation state
+    # ############################################################
     def continue_running(self) -> bool:
         if self.config.enable_viewer:
             return self.viewer.is_running()
         else:
             return True
-    
+
     def run(self):
         loop_no_sleep_dts, loop_dts = [], []
 
@@ -209,7 +339,13 @@ class Simulator:
             self.sim_step()
 
             # Get simulation state
-            self.get_sim_state()
+            sim_state_dict = self.get_sim_state()
+
+            PRINT_SIM_STATE = False
+            if PRINT_SIM_STATE:
+                for key, value in sim_state_dict.items():
+                    print(f"{key}: {value}")
+                print()
 
             # Update viewer
             if self.config.enable_viewer:
@@ -226,17 +362,28 @@ class Simulator:
                 loop_dt = loop_no_sleep_dt + sleep_dt
             else:
                 loop_dt = loop_no_sleep_dt
-                print(f"Simulation is running slower than real time, desired FPS = {1.0 / self.config.sim_dt:.1f}, actual FPS = {1.0 / loop_dt:.1f}")
+                print(
+                    f"Simulation is running slower than real time, desired FPS = {1.0 / self.config.sim_dt:.1f}, actual FPS = {1.0 / loop_dt:.1f}"
+                )
             loop_dts.append(loop_dt)
 
             # Get FPS
-            if len(loop_dts) == 100:
+            PRINT_FPS_EVERY_N_SECONDS = 1.0
+            PRINT_FPS_EVERY_N_STEPS = int(
+                PRINT_FPS_EVERY_N_SECONDS / self.config.sim_dt
+            )
+            if len(loop_dts) == PRINT_FPS_EVERY_N_STEPS:
                 total_loop_no_sleep_dt = np.sum(loop_no_sleep_dts)
                 total_loop_dt = np.sum(loop_dts)
-                print(f"Max FPS: {100 / total_loop_no_sleep_dt:.1f}")
-                print(f"FPS: {100 / total_loop_dt:.1f}")
+                print(
+                    f"Max FPS: {PRINT_FPS_EVERY_N_STEPS / total_loop_no_sleep_dt:.1f}"
+                )
+                print(f"FPS: {PRINT_FPS_EVERY_N_STEPS / total_loop_dt:.1f}")
                 loop_no_sleep_dts, loop_dts = [], []
 
+    # ############################################################
+    # Properties
+    # ############################################################
     @property
     def joint_names(self) -> list[str]:
         return [self.mj_model.joint(i).name for i in range(self.mj_model.njnt)]
@@ -261,10 +408,10 @@ class Simulator:
     def body_ids(self) -> list[int]:
         return [self.mj_model.body(name=name).id for name in self.body_names]
 
+
 if __name__ == "__main__":
     simulator_config = SimulatorConfig(
         enable_viewer=True,
     )
     simulator = Simulator(simulator_config)
     simulator.run()
-
