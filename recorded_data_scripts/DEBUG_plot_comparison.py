0fe916eb5e43875671bdf4dd8d8766a04a0c1575
Tyler Lum <tylergwlum@gmail.com>
2025-10-24 13:34:41 -0700

    Add simpler script

---
+++ recorded_data_scripts/DEBUG_plot_comparison.py
@@ -0,0 +1,23 @@
+from recorded_data_scripts.plot_recorded_data_comparison import plot_values_at_one_time_index, plot_values_over_time, plot_comparison, visualize_fig
+from recorded_data_scripts.recorded_data import RecordedData, OBS_NAMES, JOINT_NAMES_ISAACGYM, ACTION_NAMES
+import numpy as np
+
+N_OBS = len(OBS_NAMES)
+NAME_TO_OBSERVATIONS = {
+    "v1": np.random.randn(N_OBS),
+    "v2": np.random.randn(N_OBS),
+}
+
+for obs in NAME_TO_OBSERVATIONS.values():
+    assert obs.shape == (len(OBS_NAMES),), f"Expected observations to have shape (len(OBS_NAMES),) = ({len(OBS_NAMES)},), got {obs.shape}"
+
+obs_fig = plot_comparison(
+    filename_to_y={
+        name: obs
+        for name, obs in NAME_TO_OBSERVATIONS.items()
+    },
+    title="Observations",
+    yaxis_title="Obs Value",
+    y_names=OBS_NAMES,
+)
+visualize_fig(obs_fig, save_html=True)
