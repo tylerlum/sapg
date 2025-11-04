addf0ecbc6b1d273569c979f978742ee14606638
Tyler Lum <tylergwlum@gmail.com>
2025-11-04 10:29:46 -0800

    Add tyler_scratch/DEBUG_rotate_hairbrush.py

---
+++ tyler_scratch/DEBUG_rotate_hairbrush.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+import trimesh
+import numpy as np
+from scipy.spatial.transform import Rotation
+
+orig_obj_path = Path("/juno/u/tylerlum/github_repos/InstantMesh/outputs/instant-mesh-large/meshes/Brush2_Masked.obj")
+new_obj_path = Path("/home/tylerlum/github_repos/sapg/assets/urdf/tyler_objects/hairbrush/hairbrush.obj")
+assert orig_obj_path.exists(), f"Object file {orig_obj_path} does not exist"
+print(f"Loading object from {orig_obj_path}")
+orig_obj = trimesh.load(orig_obj_path)
+
+# new_x = orig_y
+# new_y = -orig_z
+# new_z = -orig_x
+T = np.eye(4)
+R = Rotation.from_euler('zx', [-115, 180], degrees=True).as_matrix()
+T[:3, :3] = R
+
+rotated_obj = orig_obj.apply_transform(T)
+
+T2 = np.eye(4)
+T2[:3, 3] = np.array([0.05, 0.0, 0.0075])
+rotated_obj = rotated_obj.apply_transform(T2)
+
+# save the rotated obj
+new_obj_path.parent.mkdir(parents=True, exist_ok=True)
+rotated_obj.export(new_obj_path)
+print(f"Saved object to {new_obj_path}")
