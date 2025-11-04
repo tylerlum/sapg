a08ba6dc51806e0f119be47c9d8d1042d0a2c949
Tyler Lum <tylergwlum@gmail.com>
2025-11-04 14:03:23 -0800

    Add roller

--- isaacgymenvs/tasks/allegro_kuka/allegro_kuka_base.py
+++ isaacgymenvs/tasks/allegro_kuka/allegro_kuka_base.py
@@ -708,6 +708,12 @@ class AllegroKukaBase(VecTask):
                 need_vhacd=True,
                 fixed_trajectory=get_hammer_trajectory(init_state, device=self.device),
             ),
+            "roller": Hammer(
+                file=str(root_dir / "assets/urdf/tyler_objects/roller/roller.urdf"),
+                scale=[3.0, 0.5, 0.5],
+                need_vhacd=True,
+                fixed_trajectory=get_hammer_trajectory(init_state, device=self.device),
+            ),
         }
         for hammer in name_to_hammer_dict.values():
             assert Path(hammer.file).exists(), f"Hammer file {hammer.file} does not exist"
@@ -816,6 +822,12 @@ class AllegroKukaBase(VecTask):
             need_vhacds = [name_to_hammer_dict["mallet"].need_vhacd]
             if USE_FIXED_SET_OF_GOAL_STATES:
                 self.trajectory_states = name_to_hammer_dict["mallet"].fixed_trajectory
+        elif object_type == "roller":
+            object_asset_files = [name_to_hammer_dict["roller"].file]
+            object_asset_scales = [name_to_hammer_dict["roller"].scale]
+            need_vhacds = [name_to_hammer_dict["roller"].need_vhacd]
+            if USE_FIXED_SET_OF_GOAL_STATES:
+                self.trajectory_states = name_to_hammer_dict["roller"].fixed_trajectory
         elif object_type == "all_hammers":
             object_asset_files = [hammer.file for hammer in name_to_hammer_dict.values()]
             object_asset_scales = [hammer.scale for hammer in name_to_hammer_dict.values()]
