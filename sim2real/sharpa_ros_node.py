5d60963eae80776eafd58f22d3edd5766622f56c
Tyler Lum <tylergwlum@gmail.com>
2025-11-09 13:47:01 -0800

    Add first version of sharpa ros node

---
+++ sim2real/sharpa_ros_node.py
@@ -0,0 +1,185 @@
+from pathlib import Path
+SHARPA_SDK_PATH = Path("/juno/u/tylerlum/Sharpa/SDK/SharpaWaveSDK_4.3.4/python")
+assert SHARPA_SDK_PATH.exists(), f"SHARPA_SDK_PATH: {SHARPA_SDK_PATH} does not exist"
+
+import sys
+# import sharpa.so from SharpaWaveSDK python folder
+sys.path.insert(0, str(SHARPA_SDK_PATH))
+
+import rospy
+from sensor_msgs.msg import JointState
+import numpy as np
+
+import math
+import time
+from sharpa import (
+    SharpaWave,
+    SharpaWaveManager,
+    ControlMode,
+    ControlSource,
+    HandSide,
+    ErrorCode
+)
+
+# Joint names (indices 0..21)
+JOINT_NAMES = [
+    "Thumb CMC Flexion/Extension",
+    "Thumb CMC Abduction/Adduction", 
+    "Thumb MCP Flexion/Extension",
+    "Thumb MCP Abduction/Adduction",
+    "Thumb DIP Flexion/Extension",
+    "Index MCP Flexion/Extension",
+    "Index MCP Abduction/Adduction",
+    "Index PIP Flexion/Extension", 
+    "Index DIP Flexion/Extension",
+    "Middle MCP Flexion/Extension",
+    "Middle MCP Abduction/Adduction",
+    "Middle PIP Flexion/Extension",
+    "Middle DIP Flexion/Extension", 
+    "Ring MCP Flexion/Extension",
+    "Ring MCP Abduction/Adduction",
+    "Ring PIP Flexion/Extension",
+    "Ring DIP Flexion/Extension",
+    "Pinky CMC Flexion/Extension",
+    "Pinky MCP Flexion/Extension",
+    "Pinky MCP Abduction/Adduction", 
+    "Pinky PIP Flexion/Extension",
+    "Pinky DIP Flexion/Extension",
+]
+
+# Angle ranges (in degrees)
+ANGLE_RANGES = [
+    (0, 50),   # Thumb CMC Flexion/Extension
+    (0, 10),     # Thumb CMC Abduction/Adduction
+    (0, 30),   # Thumb MCP Flexion/Extension
+    (0, 10),     # Thumb MCP Abduction/Adduction
+    (0, 40),     # Thumb DIP Flexion/Extension
+    (0, 20),   # Index MCP Flexion/Extension
+    (-20, 20),   # Index MCP Abduction/Adduction
+    (0, 20),     # Index PIP Flexion/Extension
+    (0, 20),     # Index DIP Flexion/Extension
+    (0, 20),   # Middle MCP Flexion/Extension
+    (-20, 20),   # Middle MCP Abduction/Adduction
+    (0, 20),     # Middle PIP Flexion/Extension
+    (0, 20),     # Middle DIP Flexion/Extension
+    (0, 20),   # Ring MCP Flexion/Extension
+    (-20, 20),   # Ring MCP Abduction/Adduction
+    (0, 20),     # Ring PIP Flexion/Extension
+    (0, 20),     # Ring DIP Flexion/Extension
+    (0, 10),     # Pinky CMC Flexion/Extension
+    (0, 20),   # Pinky MCP Flexion/Extension
+    (-20, 20),   # Pinky MCP Abduction/Adduction
+    (0, 20),     # Pinky PIP Flexion/Extension
+    (0, 20),     # Pinky DIP Flexion/Extension
+]
+
+def auto_detect_hand() -> None:
+    """Automatically detect device and return device and device serial number"""
+    print("Searching for devices...")
+    
+    try:
+        manager = SharpaWaveManager.get_instance()
+        time.sleep(1)  # Wait for 1 seconds for device discovery to complete
+        while True:
+            devices = manager.get_all_device_sn()
+            if not devices:
+                print("No available devices found")
+                time.sleep(1)
+                continue
+            else:
+                print(f"Device found: {devices[0]}")
+                return manager.connect(devices[0])
+    except Exception as e:
+        print(f"Failed to connect to device: {str(e)}")
+        exit(1)
+    
+def initialize(hand) -> bool:
+    error = hand.set_control_mode(ControlMode.POSITION)
+    if error.code != 0:
+        print(f"Failed to set control mode: {error.message}")
+        return False
+    error = hand.set_speed_coeff(0.3)
+    if error.code != 0:
+        print(f"Failed to set speed coeff: {error.message}")
+        return False
+
+    error = hand.set_current_coeff(0.6)
+    if error.code != 0:
+        print(f"Failed to set current coeff: {error.message}")
+        return False
+    error = hand.set_control_source(ControlSource.SDK)
+    if error.code != 0:
+        print(f"Failed to set control source: {error.message}")
+        return False
+    return True
+
+
+
+class SharpaRosNode:
+    def __init__(self):
+        rospy.init_node("sharpa_ros_node")
+
+        self.rate_hz = 100
+        self.dt = 1 / self.rate_hz
+        self.rate = rospy.Rate(self.rate_hz)
+
+        # Store latest cmd
+        self.latest_cmd = None
+
+        # Listen to joint cmd
+        self.joint_cmd_sub = rospy.Subscriber(
+            "/sharpa/joint_cmd", JointState, self.joint_cmd_callback
+        )
+
+        # Publish joint states
+        self.joint_states_pub = rospy.Publisher(
+            "/sharpa/joint_states", JointState, queue_size=10
+        )
+
+        # Initialize SharpaWave
+        self._setup_sharpa_wave()
+
+    def _setup_sharpa_wave(self):
+        print("Sharpa Wave Example - Starting")
+        # Try to automatically detect device
+        self.hand = auto_detect_hand()
+        if self.hand is None:
+            print("Error: No available device found")
+            exit(1)
+        print("Sharpa Wave Example - Init Hand Running Mode")
+        if not initialize(self.hand):
+            print("Error: Failed to initialize hand")
+            exit(1)
+
+        print("Sharpa Wave Example - Run Demo Gestures")
+        self.hand.start()
+
+        enable_interpolation_mode = True
+        self.hand.set_joint_position([0.0] * 22, enable_interpolation_mode)
+
+    def joint_cmd_callback(self, msg: JointState):
+        self.latest_cmd = msg
+
+    def publish_joint_states(self):
+        joint_states_msg = JointState()
+        joint_states_msg.header.stamp = rospy.Time.now()
+        joint_states_msg.name = JOINT_NAMES
+        joint_states_msg.position = np.deg2rad(self.hand.get_joint_position_degree()).tolist()
+        self.joint_states_pub.publish(joint_states_msg)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.publish_joint_states()
+            self.rate.sleep()
+
+        print("Sharpa Wave Example - Stop Hand Running Mode")
+        self.hand.stop()
+        SharpaWaveManager.get_instance().disconnect_all()
+        print("Sharpa Wave Example - Stopped")
+
+def main():
+    node = SharpaRosNode()
+    # node.run()
+
+if __name__ == "__main__":
+    main()
