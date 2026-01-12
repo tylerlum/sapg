fdbab64d86ad3dea3d9eac147bc9e925fdf8b8cb
Tyler Lum <tylergwlum@gmail.com>
2026-01-11 19:27:15 -0800

    Working whiteboard eraser

--- sim2real/rl_policy_ros_node_sharpa.py
+++ sim2real/rl_policy_ros_node_sharpa.py
@@ -942,7 +942,7 @@ if __name__ == "__main__":
             # object_scales=np.array([0.121277, 0.015, 0.015]) * 25,  # 040 large marker (smaller)
             # object_scales=np.array([0.12965531, 0.0337145 , 0.06038587]) * 25,  # whiteboard eraser
             # object_scales=np.array([0.15954332, 0.0777093 , 0.01231273]) * 25,  # iphone15pro
-            # object_scales=np.array(NAME_TO_OBJECT["whiteboard_eraser"].scale),
+            object_scales=np.array(NAME_TO_OBJECT["whiteboard_eraser"].scale),
             # object_scales=np.array(NAME_TO_OBJECT["mallet"].scale),
             # object_scales=np.array(NAME_TO_OBJECT["hammer_2"].scale),
             # object_scales=np.array(NAME_TO_OBJECT["hammer_2"].scale) * 0.75,
@@ -953,7 +953,7 @@ if __name__ == "__main__":
             # object_scales=np.array(NAME_TO_OBJECT["black_spatula"].scale),
             # object_scales=np.array(NAME_TO_OBJECT["black_spatula"].scale) * 0.8,
             # object_scales=np.array(NAME_TO_OBJECT["real_flat_screwdriver"].scale) * 0.8,
-            object_scales=np.array(NAME_TO_OBJECT["real_flat_screwdriver"].scale),
+            # object_scales=np.array(NAME_TO_OBJECT["real_flat_screwdriver"].scale),
             # object_scales=np.array([0.25, 0.02, 0.015]) * 25,  # scanned hammer 2
             # object_scales=np.array([0.25, 0.03, 0.02]) * 25,  # scanned hammer 2
             # save_foldername=None,
