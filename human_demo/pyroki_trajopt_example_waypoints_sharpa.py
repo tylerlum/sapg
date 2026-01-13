0651fb53f34a5ea8f9e9baa44a3826952ee7f43d
Tyler Lum <tylergwlum@gmail.com>
2026-01-13 14:39:15 -0800

    USE_CACHE

--- human_demo/pyroki_trajopt_example_waypoints_sharpa.py
+++ human_demo/pyroki_trajopt_example_waypoints_sharpa.py
@@ -2,10 +2,13 @@
 
 
 # Cache
-import jax
-jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
-jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
-jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+# Seems like 37s instead of 57s
+USE_CACHE = True
+if USE_CACHE:
+    import jax
+    jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+    jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+    jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
 
 import time
 import numpy as np
