e309c61101c62ed63472b87b00a08d410dae9c7d
Tyler Lum <tylergwlum@gmail.com>
2026-01-13 19:20:27 -0800

    Add hand

--- pyroki_tests/07_trajopt_improved_waypoints_sharpa.py
+++ pyroki_tests/07_trajopt_improved_waypoints_sharpa.py
@@ -255,8 +255,8 @@ def main(robot_name: Literal["ur5", "panda", "sharpa"] = "sharpa"):
     q_hands = np.concatenate([first_q_hand[None].repeat(BUFFER, axis=0), q_hands])
     print(f"traj.shape: {traj.shape}, q_hands.shape: {q_hands.shape}")
     assert len(traj) == len(q_hands), f"len(traj): {len(traj)}, len(q_hands): {len(q_hands)}"
-    traj = interpolate_traj(traj=traj, n_steps=10)
-    q_hands = interpolate_traj(traj=q_hands, n_steps=10)
+    # traj = interpolate_traj(traj=traj, n_steps=10)
+    # q_hands = interpolate_traj(traj=q_hands, n_steps=10)
 
     # Visualize trajectory
     slider = server.gui.add_slider(
@@ -283,8 +283,7 @@ def main(robot_name: Literal["ur5", "panda", "sharpa"] = "sharpa"):
 
         end_time = time.time()
         loop_dt = end_time - start_time
-        # sleep_dt = dt - loop_dt
-        sleep_dt = 0
+        sleep_dt = dt - loop_dt
         if sleep_dt > 0:
             time.sleep(sleep_dt)
         else:
