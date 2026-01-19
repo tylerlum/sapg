d593492cf9556e55686ba77770f7bf765cf99de7
kushal2000 <kushal.k.2000@gmail.com>
2026-01-18 21:30:55 -0500

    Add ablation scripts

---
+++ ablation_scripts/ours.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+seed=0
+CUSTOM_EXPERIMENT_NAME="OURS"
+WANDB_GROUP="1x_OURS"
+
+WANDB_ENTITY="kk837"
+WANDB_PROJECT="PAPER_ABLATIONS"
+
+DATETIME=$(date +"%Y-%m-%d_%H-%M-%S")
+EXPERIMENT_NAME="${CUSTOM_EXPERIMENT_NAME}_$DATETIME"
+HYDRA_RUN_DIR=./train_dir/${WANDB_PROJECT}/${WANDB_GROUP}/${EXPERIMENT_NAME}
+
+python -m isaacgymenvs.train \
+task/env=reorientation \
+++task.env.useSparseReward=False \
+headless=True \
+task.env.numEnvs=24576 \
+train.params.config.minibatch_size=98304 \
+multi_gpu=False \
+train.params.config.good_reset_boundary=0 \
+task.env.goodResetBoundary=0 \
+train.params.config.use_others_experience=lf \
+train.params.config.off_policy_ratio=1.0 \
+train.params.config.expl_type=mixed_expl_learn_param \
+train.params.config.expl_reward_type=entropy \
+train.params.config.expl_coef_block_size=4096 \
+train.params.config.expl_reward_coef_scale=0.005 \
+train.params.network.space.continuous.fixed_sigma=coef_cond \
+wandb_project=${WANDB_PROJECT} \
+wandb_entity=${WANDB_ENTITY} \
+wandb_activate=True \
+wandb_group=${WANDB_GROUP} \
+wandb_tags=[] \
+++wandb_notes='' \
+seed=${seed} \
+experiment=00_${EXPERIMENT_NAME} \
+hydra.run.dir=${HYDRA_RUN_DIR} \
+task=AllegroKukaLSTMAsymmetric \
+task.env.successTolerance=0.05 \
+task.env.targetSuccessTolerance=0.05 \
