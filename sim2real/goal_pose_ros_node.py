f217d5cdf23e587cb7315632f4eda3c9c0885ada
Tyler Lum <tylergwlum@gmail.com>
2026-01-22 16:54:47 -0800

    Switch to top and remove 5cm offset in x

--- sim2real/goal_pose_ros_node.py
+++ sim2real/goal_pose_ros_node.py
@@ -248,8 +248,8 @@ def main():
     # object_name = "real_flat_screwdriver"
     # object_name = "black_screwdriver"
     object_name = "red_screwdriver"
-    # trajectory_name = "top"
-    trajectory_name = "side"
+    trajectory_name = "top"
+    # trajectory_name = "side"
 
     # object_type = "eraser"
     # object_name = "whiteboard_eraser"
@@ -298,7 +298,7 @@ def main():
     # goals_robot_frame = [[x - 0.05, y - 0.8, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
 
     # goals_robot_frame = [[x, y - 0.8 - 0.05, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
-    goals_robot_frame = [[x - 0.05, y - 0.8 - 0.05, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
+    goals_robot_frame = [[x, y - 0.8 - 0.05, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
     # goals_robot_frame = [[x, y - 0.8, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
     # goals_robot_frame = [[x, y - 0.8, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
     # goals_robot_frame = [[x, y - 0.8 - 0.05, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
