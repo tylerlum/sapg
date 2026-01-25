e4663351ffd8c3a5bbb3630c27240fe36f7b3971
Tyler Lum <tylergwlum@gmail.com>
2026-01-25 15:13:39 -0800

    3cm tolerance

--- sim2real/goal_pose_ros_node.py
+++ sim2real/goal_pose_ros_node.py
@@ -344,8 +344,8 @@ def main():
             # object_scales=np.array([0.12965531, 0.0337145 , 0.06038587]) * 25,  # whiteboard eraser
             # object_scales=np.array([0.15954332, 0.0777093 , 0.01231273]) * 25,  # iphone15pro
             # success_threshold=0.0,
-            success_threshold=0.02,
-            # success_threshold=0.03,
+            # success_threshold=0.02,
+            success_threshold=0.03,
             success_steps=1,
             # success_threshold=10.0,
             # success_steps=30,
