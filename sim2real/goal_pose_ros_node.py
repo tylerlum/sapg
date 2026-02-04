d0602a4a89a53abaceabe752389f3c7d2fc41f53
Tyler Lum <tylergwlum@gmail.com>
2026-02-03 19:54:52 -0800

    1cm tolerance and 1.5cm offset from whiteboard

--- sim2real/goal_pose_ros_node.py
+++ sim2real/goal_pose_ros_node.py
@@ -299,7 +299,7 @@ def main():
     goals_robot_frame = [[x, y - 0.8, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
 
     # Additional offsets
-    goals_robot_frame = [[x - 0.05, y, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
+    goals_robot_frame = [[x - 0.015, y, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
 
     DOWNSAMPLE_FACTOR = 10
     # DOWNSAMPLE_FACTOR = 1
@@ -351,7 +351,8 @@ def main():
             # object_scales=np.array([0.12965531, 0.0337145 , 0.06038587]) * 25,  # whiteboard eraser
             # object_scales=np.array([0.15954332, 0.0777093 , 0.01231273]) * 25,  # iphone15pro
             # success_threshold=0.0,
-            success_threshold=0.02,
+            success_threshold=0.01,
+            # success_threshold=0.02,
             # success_threshold=0.03,
             # success_threshold=0.05,
             success_steps=1,
