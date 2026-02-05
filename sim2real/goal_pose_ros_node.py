08f1287a81f5ec892a7bf25c9beabd82438626f3
Tyler Lum <tylergwlum@gmail.com>
2026-02-04 17:37:21 -0800

    spoon_spatula serve_strawberry_easy no offsets 2cm tolerance

--- sim2real/goal_pose_ros_node.py
+++ sim2real/goal_pose_ros_node.py
@@ -251,18 +251,18 @@ def main():
     # trajectory_name = "side_swing_new_shifted"
     # trajectory_name = "down_swing_shifted"
 
-    # object_type = "spatula"
+    object_type = "spatula"
     # object_name = "black_spatula"
-    # object_name = "spoon_spatula"
-    # trajectory_name = "serve_plate"
+    object_name = "spoon_spatula"
+    trajectory_name = "serve_strawberry_easy"
     # trajectory_name = "flip_pancake"
 
-    object_type = "screwdriver"
-    object_name = "real_flat_screwdriver"
+    # object_type = "screwdriver"
+    # object_name = "real_flat_screwdriver"
     # object_name = "black_screwdriver"
     # object_name = "red_screwdriver"
     # trajectory_name = "top"
-    trajectory_name = "side"
+    # trajectory_name = "side"
     # trajectory_name = "clockwise_top"
     # trajectory_name = "clockwise_side"
 
@@ -304,7 +304,7 @@ def main():
     goals_robot_frame = [[x, y - 0.8, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
 
     # Additional offsets
-    goals_robot_frame = [[x - 0.05, y, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
+    # goals_robot_frame = [[x - 0.05, y, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
     # goals_robot_frame = [[x, y - 0.05, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
 
     # goals_robot_frame = [[x - 0.0125, y, z - 0.015, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
@@ -366,8 +366,8 @@ def main():
             # object_scales=np.array([0.15954332, 0.0777093 , 0.01231273]) * 25,  # iphone15pro
             # success_threshold=0.0,
             # success_threshold=0.01,
-            # success_threshold=0.02,
-            success_threshold=0.03,
+            success_threshold=0.02,
+            # success_threshold=0.03,
             # success_threshold=0.05,
             success_steps=1,
             # success_steps=10,
