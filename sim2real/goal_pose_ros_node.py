7ee6e6d2dec5d8ac2bb15ab036a484984564e16e
Tyler Lum <tylergwlum@gmail.com>
2026-02-04 16:31:09 -0800

    Retry top traj with 5cm offset in y

--- sim2real/goal_pose_ros_node.py
+++ sim2real/goal_pose_ros_node.py
@@ -261,7 +261,8 @@ def main():
     # object_name = "real_flat_screwdriver"
     # object_name = "black_screwdriver"
     object_name = "red_screwdriver"
-    trajectory_name = "clockwise_top"
+    trajectory_name = "top"
+    # trajectory_name = "clockwise_top"
     # trajectory_name = "clockwise_side"
 
     # object_type = "eraser"
@@ -302,6 +303,8 @@ def main():
     goals_robot_frame = [[x, y - 0.8, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
 
     # Additional offsets
+    goals_robot_frame = [[x, y - 0.05, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
+
     # goals_robot_frame = [[x - 0.0125, y, z - 0.015, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
     # goals_robot_frame = [[x - 0.01, y, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
     # goals_robot_frame = [[x + 0.02, y, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
