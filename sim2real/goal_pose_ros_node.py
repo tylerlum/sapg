2a112de4492df4b3cca12c658a1ec508b9567ad7
Tyler Lum <tylergwlum@gmail.com>
2026-02-09 21:29:26 -0800

    Lower z by 2cm, very good

--- sim2real/goal_pose_ros_node.py
+++ sim2real/goal_pose_ros_node.py
@@ -313,7 +313,8 @@ def main():
     goals_robot_frame = [[x, y - 0.8, z, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_world_frame]
 
     # Additional offsets
-    goals_robot_frame = [[x, y, z - 0.01, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
+    # goals_robot_frame = [[x, y, z - 0.01, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
+    goals_robot_frame = [[x, y, z - 0.02, qx, qy, qz, qw] for x, y, z, qx, qy, qz, qw in goals_robot_frame]
 
     DOWNSAMPLE_FACTOR = 10
     # DOWNSAMPLE_FACTOR = 1
